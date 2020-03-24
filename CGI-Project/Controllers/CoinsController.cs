04b02cf03f7151de9310171c365008ff2e241f23
Emirhan <Demirci.Emirhan@outlook.com>
2020-03-24 15:58:40 +0100

    model added

---
+++ CGI-Project/Controllers/CoinsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using BitcoGG_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BitcoGG_API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoinsController : ControllerBase
+    {
+        private static string API_KEY = "e59bd4f0-b6d4-4750-a58c-7c4357a6534f";
+        private Coins _coins;
+        public CoinsController(Coins coins)
+        {
+            _coins = coins;
+        }
+
+       [HttpGet]
+        public Object GetCoins()
+        {
+            var URL = new UriBuilder("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
+
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            queryString["start"] = "1";
+            queryString["limit"] = "5000";
+            queryString["convert"] = "USD";
+
+            URL.Query = queryString.ToString();
+
+            var client = new WebClient();
+            client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY);
+            client.Headers.Add("Accepts", "application/json");
+            return client.DownloadString(URL.ToString());
+        }
+    }
+
+
+}
---
+++ CGI-Project/Models/Coins.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BitcoGG_API.Models
+{
+    public class Coins
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string symbol { get; set; }
+        public int num_market_pairs { get; set; }
+    }
+}
