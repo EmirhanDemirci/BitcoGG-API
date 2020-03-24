62e0fb98c9587bf160af08554a1de75a24348e58
Emirhan Demirci <Demirci.Emirhan@outlook.com>
2020-03-24 16:46:38 +0100

    Crryptomunten geladen binnen het API

--- CGI-Project/Controllers/CoinsController.cs
+++ CGI-Project/Controllers/CoinsController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
 using BitcoGG_API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace BitcoGG_API.Controllers
 {
@@ -16,28 +18,28 @@ namespace BitcoGG_API.Controllers
     public class CoinsController : ControllerBase
     {
         private static string API_KEY = "e59bd4f0-b6d4-4750-a58c-7c4357a6534f";
-        private Coins _coins;
-        public CoinsController(Coins coins)
-        {
-            _coins = coins;
-        }
+        //private Coins _coins;
+        //public CoinsController(Coins coins)
+        //{
+        //    _coins = coins;
+        //}
 
        [HttpGet]
-        public Object GetCoins()
+        public async Task<IActionResult> GetCoins()
         {
-            var URL = new UriBuilder("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
-
-            var queryString = HttpUtility.ParseQueryString(string.Empty);
-            queryString["start"] = "1";
-            queryString["limit"] = "5000";
-            queryString["convert"] = "USD";
-
-            URL.Query = queryString.ToString();
-
-            var client = new WebClient();
-            client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY);
-            client.Headers.Add("Accepts", "application/json");
-            return client.DownloadString(URL.ToString());
+             Coins.RootObject reservationList = new Coins.RootObject();
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("X-CMC_PRO_API_KEY", API_KEY);
+                httpClient.DefaultRequestHeaders.Add("Accepts", "application/json");
+                using (var response = await httpClient.GetAsync("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    reservationList = JsonConvert.DeserializeObject<Coins.RootObject>(apiResponse);
+                }
+                
+            }
+            return Ok(reservationList); 
         }
     }
 
